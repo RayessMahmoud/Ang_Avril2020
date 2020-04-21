0ff9fca8c45362f75da188822368e2ff65b45813
RayessMahmoud <mahmoud.rayess@hotmail.com>
2020-04-21 11:59:45 +0100

    etap 1 router

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageNotFountComponent } from './pages/page-not-fount/page-not-fount.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageNotFountComponent],
   imports: [
     CommonModule
   ]
