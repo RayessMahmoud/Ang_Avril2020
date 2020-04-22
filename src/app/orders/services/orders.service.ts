f961dd0b2a6f6dccc410676a1ecccaba9964c02f
RayessMahmoud <mahmoud.rayess@hotmail.com>
2020-04-22 10:23:51 +0100

    recup donne pipe affichage donne

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Order } from 'src/app/shared/models/order';
-import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -16,7 +17,13 @@ export class OrdersService {
 
 
   constructor(private http: HttpClient) {
-    this.collection = this.http.get<Order[]>(`${this.urlApi}orders`);
+    this.collection = this.http.get<Order[]>(`${this.urlApi}orders`).pipe(
+      map((tab)=>{
+        return tab.map((obj)=>{
+          return new Order(obj);
+        });
+      })
+    );
   }
 
 
